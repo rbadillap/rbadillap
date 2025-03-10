f305b960669bb5f41f4ea2cd87f887de0795cad6
Ronny Badilla <info@ronnybadilla.com>
2025-03-10 13:13:02 -0600

    fix lint warnings

--- app/branding/page.tsx
+++ app/branding/page.tsx
@@ -4,7 +4,6 @@ import { useEffect, useRef, useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { RootLayout } from "@/components/layout/RootLayout"
-import { Metadata } from 'next'
 
 export default function BrandingPage() {
   // References to canvas elements
