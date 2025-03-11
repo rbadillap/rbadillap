b93065f4b04c02696aa28f09c80845b7ab97bd23
Ronny Badilla <info@ronnybadilla.com>
2025-03-11 11:14:26 -0600

    fix lint warnings

--- components/home/HomeContent.tsx
+++ components/home/HomeContent.tsx
@@ -2,7 +2,6 @@
 
 import React from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { Button } from "@/components/ui/button"
 import { CommandIcon } from "lucide-react"
 
 export function HomeContent() {
